1367edc8943c49d2795b8ed9eefefc9c52b43e41
XiangXin <ictxiangxin@gmail.com>
2014-10-10 16:15:23 +0800

    add feature test

---
+++ test/feature.py
@@ -0,0 +1,38 @@
+__author__ = 'ict'
+
+import DAL.yahoo  # 读取yahoo股票数据
+import calculate.feature.core
+
+# 这段代码测试所有特征的计算
+
+# 读取存放在e盘下的yahoo股票数据，使用数据中的所有价格数据。
+data = DAL.yahoo.csv(r"e:\stockdata", "open high low close adjclose")
+
+# 选择要计算的所有特征名称
+mtd_str = "MA, MA cross, price return, ratio, RSV"
+
+# 针对每个特征计算方法的参数
+option = [
+    {
+        "w": 10,
+        "offset": 0
+    },
+    {
+        "x": 3,
+        "y": 5,
+        "offset": 0,
+    },
+    None,
+    None,
+    {
+        "n": 10,
+        "ohlc": (0, 1, 2, 3),
+    }
+]
+
+# 所有非multidata的方法的main值
+main = [4, 4, 4, 4, None]
+
+# 计算每个数据的所有特征并打印
+for key, value in data.items():
+    print(calculate.feature.core.method(mtd_str, value, option, main))
