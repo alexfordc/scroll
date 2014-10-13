11aa8fb492a981b68e54d0492a030bae4c63deca
XiangXin <ictxiangxin@gmail.com>
2014-10-13 20:37:41 +0800

    new complete cluster algorithm

--- calculate/cluster/graph/complete_graph.py
+++ calculate/cluster/graph/complete_graph.py
@@ -3,27 +3,20 @@ __author__ = 'ict'
 
 def compute(graph):
     links = graph.nodes_link()
-    node_mcg = {}
+    node_mcg = set()
     for node in graph.get_node():
         node_link = set(links[node])
-        while len(node_link) != 0:
+        for link in node_link:
             tmp_set = set()
             tmp_set.add(node)
-            if node in node_link:
-                node_link.remove(node)
+            tmp_set.add(link)
             tmp_link = set(node_link)
-            for other_node in tmp_link.copy():
+            tmp_link.remove(link)
+            while True:
+                if len(tmp_link) == 0:
+                    break
+                other_node = tmp_link.pop()
                 tmp_set.add(other_node)
-                if other_node in node_link:
-                    node_link.remove(other_node)
-                if node in links[other_node]:
-                    links[other_node].remove(node)
                 tmp_link &= links[other_node]
-            if node not in node_mcg:
-                node_mcg[node] = set()
-            if len(tmp_set) > len(node_mcg[node]):
-                node_mcg[node] = tmp_set
-    rst = set()
-    for _, mcg in node_mcg.items():
-        rst.add(frozenset(mcg))
-    return rst
+            node_mcg.add(frozenset(tmp_set))
+    return node_mcg
