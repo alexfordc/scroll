5399bfe6e0d4fc99cc1c89a1ccf4957b955fe5d2
XiangXin <ictxiangxin@gmail.com>
2014-10-13 12:35:08 +0800

    bug fix: multidata_list

--- calculate/feature/KDJ.py
+++ calculate/feature/KDJ.py
@@ -5,7 +5,7 @@ import calculate.feature.SMA
 
 
 def compute(multidata_list, n, m1, m2, ohlc=(0, 1, 2, 3)):
-    rsv_list = calculate.feature.RSV.compute(data_list, n, ohlc)
+    rsv_list = calculate.feature.RSV.compute(multidata_list, n, ohlc)
     k = calculate.feature.SMA.compute(rsv_list, m1, 1)
     d = [50] + calculate.feature.SMA.compute(k, m2, 1)
-    return [[k[i], d[i], 3 * k[i] - 2 * d[i]] for i in range(len(data_list))]
+    return [[k[i], d[i], 3 * k[i] - 2 * d[i]] for i in range(len(multidata_list))]
