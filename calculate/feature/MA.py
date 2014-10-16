0b0d6878fd5e4c6dedd9679bc53360d530435349
ictxiangxin <ictxiangxin@gmail.com>
2014-10-16 11:05:36 +0800

    bug fix: MA

--- calculate/feature/MA.py
+++ calculate/feature/MA.py
@@ -3,8 +3,8 @@ __author__ = 'ict'
 
 def compute(data_list, w, offset=0, start=None):
     if start is None:
-        start = w
+        start = w - 1
     ma_list = []
-    for i in range(start - offset - 1, len(data_list) - offset):
+    for i in range(start - offset, len(data_list) - offset):
         ma_list.append(sum(data_list[i - w + 1 + offset: i + 1 + offset]) / w)
     return ma_list
