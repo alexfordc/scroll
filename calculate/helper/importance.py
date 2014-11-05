6ce8b3eeb3b309fe866d9c2ccd4fb159ac3fffe7
XiangXin <ictxiangxin@gmail.com>
2014-11-05 22:21:14 +0800

    add many method for importance

--- calculate/helper/importance.py
+++ calculate/helper/importance.py
@@ -1,14 +1,25 @@
 __author__ = 'ict'
 
 from calculate.helper.statistics import variance
+from calculate.helper.statistics import entropy
+from calculate.helper.statistics import gini
 import math
 
 
-def importance(data_dict):
+def importance(data_dict, method="standard variance"):
     weight_list = []
     tmp = data_dict.popitem()
     data_dict[tmp[0]] = tmp[1]
     for i in range(len(tmp[1])):
-        weight_list.append(variance([value[i] for _, value in data_dict.items()]))
+        if method == "standard variance":
+            weight_list.append(math.sqrt(variance([value[i] for _, value in data_dict.items()])))
+        elif method == "variance":
+            weight_list.append(variance([value[i] for _, value in data_dict.items()]))
+        elif method == "entropy":
+            weight_list.append(entropy([value[i] for _, value in data_dict.items()]))
+        elif method == "gini":
+            weight_list.append(gini([value[i] for _, value in data_dict.items()]))
+        else:
+            raise Exception("Invalid method: " + str(method))
     for key, value in data_dict.items():
-        data_dict[key] = [math.sqrt(weight_list[i]) * value[i] for i in range(len(tmp[1]))]
+        data_dict[key] = [weight_list[i] * value[i] for i in range(len(tmp[1]))]
